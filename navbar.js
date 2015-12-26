17fe74c01969bf9b197804393fc55c96306c24c4
Kamil Tomsik <info@tomsik.cz>
2015-12-26 13:48:24 +0100

    navbar: add text & href opts (navbar-brand)

--- navbar.js
+++ navbar.js
@@ -1,11 +1,14 @@
+import Element from './_element';
 import Component from './component';
 import Div from './div';
 import Container from './container';
 import css from './_css';
 
 class Navbar extends Component{
-  init({type = 'default', cls, children}){
+  init({type = 'default', text = '', href = '', cls, children}){
     this.type = type;
+    this.text = text;
+    this.href = href;
     this.cls = cls;
     this.children = children;
   }
@@ -16,6 +19,13 @@ class Navbar extends Component{
 }
 
 Navbar.tpl = [Div, {cls: '= divCls'},
+  [Element, {
+    tagName: 'a',
+    className: 'navbar-brand',
+    href: '= href',
+    innerText: '= text'
+  }],
+
   [Container, {
     children: '= children'
   }]
