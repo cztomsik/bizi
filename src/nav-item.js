80f5c3040daa7c1454b01cca92586253785176a8
Kamil Tomsik <info@tomsik.cz>
2016-01-04 19:50:13 +0100

    nav-item: rewrite as regular component

--- src/nav-item.js
+++ src/nav-item.js
@@ -1,24 +1,16 @@
-import $ from 'jquery';
+import Component from './component';
+import Element from './_element';
 
-class NavItem{
-  constructor(opts){
-    this.$el = $('<li><a></li>');
-    this.$a = this.$el.find('a');
-    this.el = this.$el[0];
-
-    this.reset(opts);
-  }
-
-  reset(opts){
-    this.$a.text(opts.text);
-    this.$a[0].onclick = opts.onClick;
-  }
-
-  destroy(){
-    this.$el.remove();
-    this.$el = null;
-    this.$a = null;
+class NavItem extends Component{
+  init({cls, text, onClick}){
+    this.cls = cls;
+    this.text = text;
+    this.onClick = onClick;
   }
 }
+
+NavItem.tpl = [Element, {tagName: 'li', className: '= cls'},
+  [Element, {tagName: 'a', innerText: '= text', onclick: '= onClick'}]
+];
 
 export default NavItem;
