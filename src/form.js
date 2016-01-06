f4ac9d030b9a0d12727c5386e6fd974b68e1999a
Kamil Tomsik <info@tomsik.cz>
2016-01-06 21:36:24 +0100

    form: add onChange

--- src/form.js
+++ src/form.js
@@ -2,15 +2,17 @@ import Component from './component';
 import Element from './_element';
 
 class Form extends Component{
-  init({children, cls}){
+  init({children, cls, onChange}){
     this.children = children;
     this.cls = cls;
+    this.onChange = onChange;
   }
 }
 
 Form.tpl = [Element, {
   tagName: 'form',
   className: '= cls',
+  onchange: '= onChange',
   children: '= children'
 }];
 
