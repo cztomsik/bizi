78dd5c5c6440470dd1ae04157b3bc824c3d8c2c0
Kamil Tomsik <info@tomsik.cz>
2016-01-24 21:49:51 +0100

    form-group: remove `label` opt

--- src/form-group.js
+++ src/form-group.js
@@ -4,39 +4,22 @@ import css from './_css';
 import ControlLabel from './control-label';
 
 class FormGroup extends Component{
-  init({size, label, cls, children}){
+  init({size, cls, children}){
     // sm/lg
     this.size = size;
-    this.label = label;
+
     this.cls = cls;
     this.children = children;
-
-    // how about making instantiate public? so we can get instance of (bound) component
-    // which will be automatically destroyed?
-    // - it doesnt have to be instance method though (Component.getManagedInstance())
-    if ( ! this._label){
-      this._label = new ControlLabel({text: this.label});
-    }
-
-    this.divChildren = [this._label].concat(this.children);
   }
 
   get divCls(){
     return css.for(this.cls, 'form-group', this.size);
   }
-
-  destroy(){
-    this._label.destroy();
-    this._label = null;
-    this.divChildren = null;
-
-    super.destroy();
-  }
 }
 
 FormGroup.tpl = [Div, {
   cls: '= divCls',
-  children: '= divChildren'
+  children: '= children'
 }];
 
 export default FormGroup;
