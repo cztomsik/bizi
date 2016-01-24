c2ddda3e80b57dd0b03101f867ae8ee1f3e853ca
Kamil Tomsik <info@tomsik.cz>
2016-01-25 00:19:25 +0100

    link: fix cursor for missing href nav-item: use link

--- src/link.js
+++ src/link.js
@@ -3,10 +3,21 @@ import Element from './_element';
 import css from './_css';
 
 class Link extends Component{
-  init({text, href, cls}){
+  init({text, href = '', cls, onClick}){
     this.text = text;
     this.href = href;
     this.cls = cls;
+    this.onClick = onClick;
+  }
+
+  click(e){
+    if (this.onClick){
+      this.onClick.call(null, e);
+    }
+
+    if ( ! this.href){
+      e.preventDefault();
+    }
   }
 }
 
@@ -14,7 +25,8 @@ Link.tpl = [Element, {
   tagName: 'a',
   href: '= href',
   innerText: '= text',
-  className: '= cls'
+  className: '= cls',
+  onclick: '() click'
 }];
 
 export default Link;
--- src/nav-item.js
+++ src/nav-item.js
@@ -1,16 +1,18 @@
 import Component from './component';
 import Element from './_element';
+import Link from './link';
 
 class NavItem extends Component{
-  init({cls, text, onClick}){
+  init({cls, text, href, onClick}){
     this.cls = cls;
     this.text = text;
+    this.href = href;
     this.onClick = onClick;
   }
 }
 
 NavItem.tpl = [Element, {tagName: 'li', className: '= cls'},
-  [Element, {tagName: 'a', innerText: '= text', onclick: '= onClick'}]
+  [Link, {text: '= text', href: '= href', onClick: '= onClick'}]
 ];
 
 export default NavItem;
