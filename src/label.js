34c95e018a955b1412bc6e0fa7d1eb96d60d14c3
Kamil Tomsik <info@tomsik.cz>
2016-02-08 01:43:26 +0100

    control-label: ControlLabel -> Label

--- src/label.js
+++ src/label.js
@@ -2,17 +2,17 @@ import Component from './component';
 import Element from './_element';
 import css from './_css';
 
-class ControlLabel extends Component{
+class Label extends Component{
   init({text, cls}){
     this.labelCls = css.for(cls, 'control-label');
     this.text = text;
   }
 }
 
-ControlLabel.tpl = [Element, {
+Label.tpl = [Element, {
   tagName: 'label',
   className: '= labelCls',
   innerText: '= text'
 }];
 
-export default ControlLabel;
+export default Label;
